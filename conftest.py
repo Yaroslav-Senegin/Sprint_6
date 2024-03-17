23bc594a7949e5464fbeaa7fffaed59a795657df
Yaroslav-Senegin <mized24@yandex.ru>
2024-03-17 22:17:11 +0700

    Fixed fixture

--- conftest.py
+++ conftest.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from utils.urls import Urls
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(scope='function')
+    driver = webdriver.Chrome()
     driver.get(Urls.MAIN_PAGE)
     
     yield driver
--- tests/test_FAQ.py
+++ tests/test_FAQ.py
@@ -1,14 +1,14 @@
 from locators.home_page_locators import HomePageLocators
 from pages.home_page import HomePage
 from conftest import driver
-from utils.test_data import AnswersText
+from utils.test_data import YaScooterHomePageFAQ
 import pytest
 import allure
 
 
 class TestQuestionsHomePage:
     @allure.title('Проверка ответов на вопросы из выпадающего списка «Вопросы о важном»')
-    @pytest.mark.parametrize('number, expected_answer', AnswersText.answers)
+    @pytest.mark.parametrize('number, expected_answer', YaScooterHomePageFAQ.answers)
     def test_question(self, driver, number, expected_answer):
         HomePage(driver).get_cookies(HomePageLocators.COOKIES_BTN)
         HomePage(driver).scroll(HomePageLocators.LAST_QUESTION)
