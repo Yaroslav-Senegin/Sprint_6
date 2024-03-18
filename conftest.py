03a7c32227928bd1df1d630ac3ee64bd09c33274
Yaroslav-Senegin <mized24@yandex.ru>
2024-03-18 08:22:45 +0700

    Fixed fixture

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from utils.urls import Urls
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get(Urls.HOME_PAGE)
     
     yield driver
